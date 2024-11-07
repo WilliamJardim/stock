0828d5cc0a11be0f065cdeda8e79c3d08ef44e5c
William Jardim <williamalvesjardim@gmail.com>
2024-11-07 11:05:54 -0300

    Criando estrutura da tabela de produtos

--- frontend-react/src/pages/ListaProdutos/index.tsx
+++ frontend-react/src/pages/ListaProdutos/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import './style.scss';
 
 const ListaProdutos: React.FC = () => {
-  return <h1>Bem-vindo à lista de produtos!</h1>;
+  return (
+    <Fragment>
+      <h1> Lista de Produtos </h1>
+      <div className='grid-produtos'>
+        
+        <div className='produto'>
+          
+          <div className="foto">
+            <h2> Imagem </h2>
+          </div>
+
+          <div className="dados">
+            <h2 className='titulo'> Tabelinha </h2>
+            <table className='tabela-dados'>
+              <tr> 
+                <td> NOME </td>
+                <td> Garfo </td>
+              </tr>
+              <tr> 
+                <td> PREÇO </td>
+                <td> R$: 12,00 </td>
+              </tr>
+              <tr> 
+                <td> ESTOQUE </td>
+                <td> 25 unidades </td>
+              </tr>
+            </table>
+          </div>
+
+        </div>
+
+
+
+      </div>
+    </Fragment>
+  );
 }
 
 export default ListaProdutos;
