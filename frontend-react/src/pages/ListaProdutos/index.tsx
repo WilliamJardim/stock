ade99a0258c34331bd2af3d9dcd52ee4666a89c8
William Jardim <williamalvesjardim@gmail.com>
2024-11-07 10:41:39 -0300

    Começando a criar as rotas do front

---
+++ frontend-react/src/pages/ListaProdutos/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListaProdutos: React.FC = () => {
+  return <h1>Bem-vindo à lista de produtos!</h1>;
+}
+
+export default ListaProdutos;
---
+++ frontend-react/src/pages/NaoEncontrado/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NaoEncontrado: React.FC = () => {
+  return <h1>404 - Página não encontrada</h1>;
+}
+
+export default NaoEncontrado;
